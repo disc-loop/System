2c90d86361dd98058b41c71f73e42539f1384c54
Tom Jones <tom@recursivediscourse.com>
2026-06-30 23:34:57 +1000

    Format system config with guix style

--- config.scm
+++ config.scm
@@ -1,28 +1,33 @@
-(use-modules 
-  (gnu)
-  (gnu packages version-control)
-  (gnu packages vim)
-  (gnu packages package-management)
-  (gnu packages ssh)
-  (gnu packages xdisorg)
-  (gnu packages shells)
-  (gnu packages terminals)
-  (gnu packages web)
-  (gnu packages tmux)
-  (gnu packages docker)
-  (gnu packages gl)
-  (nonguix transformations)
-  (nongnu packages linux)
-  (nongnu packages nvidia)
-  (nongnu packages mozilla)
-  (nongnu system linux-initrd))
-(use-service-modules cups desktop networking ssh xorg sddm docker)
+(use-modules (gnu)
+             (gnu packages version-control)
+             (gnu packages vim)
+             (gnu packages package-management)
+             (gnu packages ssh)
+             (gnu packages xdisorg)
+             (gnu packages shells)
+             (gnu packages terminals)
+             (gnu packages web)
+             (gnu packages tmux)
+             (gnu packages docker)
+             (gnu packages gl)
+             (nonguix transformations)
+             (nongnu packages linux)
+             (nongnu packages nvidia)
+             (nongnu packages mozilla)
+             (nongnu system linux-initrd))
+(use-service-modules cups
+                     desktop
+                     networking
+                     ssh
+                     xorg
+                     sddm
+                     docker)
 
-(define %my-os 
+(define %my-os
   (operating-system
     (kernel linux)
 
-    ;; Pulled this straight from nonguix to get wireless working. Might need to 
+    ;; Pulled this straight from nonguix to get wireless working. Might need to
     ;; come back and understand what it's doing.
     (kernel-arguments '("modprobe.blacklist=b43,b43legacy,ssb,bcm43xx,brcm80211,brcmfmac,brcmsmac,bcma"))
 
@@ -46,51 +51,51 @@
                     (group "users")
                     (home-directory "/home/tom")
                     (shell (file-append zsh "/bin/zsh"))
-                    (supplementary-groups '("wheel" "netdev" "audio" "video" "docker")))
-                  %base-user-accounts))
+                    (supplementary-groups '("wheel" "netdev" "audio" "video"
+                                            "docker"))) %base-user-accounts))
 
-    (packages (append 
-                (list 
-                  zsh
-                  git 
-                  openssh
-                  wl-clipboard
-                  neovim
-                  tmux
-                  fzf
-                  jq
-                  docker
-                  firefox
-                  steam-nvidia-580) 
-                  %base-packages))
+    (packages (append (list zsh
+                            git
+                            openssh
+                            wl-clipboard
+                            neovim
+                            tmux
+                            fzf
+                            jq
+                            docker
+                            firefox
+                            steam-nvidia-580) %base-packages))
 
-    (services 
-      (append 
-        (list 
-	  ;; Apparently GDM doesn't play nicely with Nvidia drivers, so we're replacing
-	  ;; it with SDDM.
-	  (service sddm-service-type)
-          (service plasma-desktop-service-type) 
-	  (service openssh-service-type)
-          (service bluetooth-service-type)
-          (service docker-service-type)
-          (service containerd-service-type)
-          ;; Increasing max open file descriptors from 1024 in case we have to build
-          ;; from source.
-          (service pam-limits-service-type 
-            (list (pam-limits-entry "*" 'both 'nofile 4096))))
-        ;; Using substitutes otherwise everything builds from scratch! 
-        (modify-services %desktop-services
-          ;; See comment above about GDM vs. SDDM.
-	  (delete gdm-service-type)
-          (guix-service-type config => (guix-configuration
-            (inherit config)
-            (substitute-urls
-             (append (list "https://substitutes.nonguix.org")
-               %default-substitute-urls))
-            (authorized-keys
-             (append (list (local-file "nonguix-signing-key.pub"))
-               %default-authorized-guix-keys)))))))
+    (services
+     (append (list
+              ;; Apparently GDM doesn't play nicely with Nvidia drivers, so we're replacing
+              ;; it with SDDM.
+              (service sddm-service-type)
+              (service plasma-desktop-service-type)
+              (service openssh-service-type)
+              (service bluetooth-service-type)
+              (service docker-service-type)
+              (service containerd-service-type)
+              ;; Increasing max open file descriptors from 1024 in case we have to build
+              ;; from source.
+              (service pam-limits-service-type
+                       (list (pam-limits-entry "*"
+                                               'both
+                                               'nofile 4096))))
+             ;; Using substitutes otherwise everything builds from scratch!
+             (modify-services %desktop-services
+               ;; See comment above about GDM vs. SDDM.
+               (delete gdm-service-type)
+               (guix-service-type config =>
+                                  (guix-configuration (inherit config)
+                                                      (substitute-urls (append
+                                                                        (list
+                                                                         "https://substitutes.nonguix.org")
+                                                                        %default-substitute-urls))
+                                                      (authorized-keys (append
+                                                                        (list (local-file
+                                                                               "nonguix-signing-key.pub"))
+                                                                        %default-authorized-guix-keys)))))))
 
     (bootloader (bootloader-configuration
                   (bootloader grub-bootloader)
@@ -98,7 +103,8 @@
                   (keyboard-layout keyboard-layout)))
 
     (mapped-devices (list (mapped-device
-                            (source (uuid "1e2e2948-5bd8-47c2-bbc0-b17bca680c61"))
+                            (source (uuid
+                                     "1e2e2948-5bd8-47c2-bbc0-b17bca680c61"))
                             (target "cryptroot")
                             (type luks-device-mapping))))
 
@@ -108,7 +114,6 @@
                            (type "ext4")
                            (dependencies mapped-devices)) %base-file-systems))))
 
-((nonguix-transformation-nvidia 
-   #:driver nvda-580
-   #:configure-xorg? sddm-service-type) 
+((nonguix-transformation-nvidia #:driver nvda-580
+                                #:configure-xorg? sddm-service-type)
  %my-os)
